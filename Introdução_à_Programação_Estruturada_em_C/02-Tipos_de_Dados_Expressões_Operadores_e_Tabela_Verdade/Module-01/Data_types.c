11260a0f3cea646757bc2ec04900f3cf1be8ed13
Cicero Eduardo <ciceroeduardobarbosa@hotmail.com>
2024-03-13 22:57:59 -0300

    add module 01 in the theme 2 in introduction in estructure programing in C

---
+++ Introdução_à_Programação_Estruturada_em_C/02-Tipos_de_Dados_Expressões_Operadores_e_Tabela_Verdade/Module-01/Data_types.c
@@ -0,0 +1,19 @@
+// https://engenhariasoftware.wordpress.com/2020/05/11/notacao-hungara-para-denominar-uma-variavel/
+#include <stdio.h>
+#include <stdbool.h> // Para usar o tipo de dados bool
+
+int main() {
+    const char *cName = "Hello World!"; //cName é um ponteiro para o primeiro de uma sequência de caracteres que formam uma string.
+    const int nQuantidade = 10;
+    const float fPi = 3.141592;
+    const bool bVerdade = true; // Use bool em vez de double para representar valores booleanos
+    const double dVerdade = 1.0;
+    
+    printf("cName: %s\n", cName);
+    printf("nQuantidade: %d\n", nQuantidade);
+    printf("fPi: %f\n", fPi);
+    printf("bVerdade: %d\n", bVerdade); // %d para imprimir um valor bool como 0 ou 1
+    printf("dVerdade: %lf\n", dVerdade);
+    
+    return 0;
+}
