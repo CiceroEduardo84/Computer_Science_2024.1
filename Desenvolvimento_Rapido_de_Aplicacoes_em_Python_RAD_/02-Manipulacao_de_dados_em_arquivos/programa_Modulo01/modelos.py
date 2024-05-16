97e6aaa2d2ab028bc0dc9b0de40748791f4a7b7c
Cicero Eduardo <ciceroeduardobarbosa@hotmail.com>
2024-05-15 23:16:31 -0300

    add string manipulation functions(Modulo 2) in the theme 02: data manipulation in files in RAD

--- Desenvolvimento_Rapido_de_Aplicacoes_em_Python_RAD_/02-Manipulacao_de_dados_em_arquivos/programa_Modulo01/modelos.py
+++ Desenvolvimento_Rapido_de_Aplicacoes_em_Python_RAD_/02-Manipulacao_de_dados_em_arquivos/programa_Modulo01/modelos.py
@@ -1,15 +1,23 @@
 class Variavel:
 
     def __init__(self, posicao_inicial, tamanho, codigo, descricao):
-        self.posicao_inicial = posicao_inicial
-        self.tamanho = tamanho
+        # Modificado
+        # self.posicao_inicial = posicao_inicial
+        # self.tamanho = tamanho
+        self.posicao_inicial = int(posicao_inicial) - 1
+        self.tamanho = int(tamanho)
         self.codigo = codigo
         self.descricao = descricao
-        self.categoria = []
+        # self.categoria = []
+        self.categoria = {}
 
     def add_categoria(self, categoria):
         # categoria = dict {'categoria_tipo': tipo, 'categoria_descricao_tipo': descricao}
-        self.categoria.append(categoria)
+        # alteracao
+        # self.categoria.append(categoria)
+        self.categoria[categoria.get('categoria_tipo')] = categoria.get('categoria_descricao_tipo')
 
     def __str__(self):
-        return self.codigo + " - " + self.descricao
+        # return self.codigo + " - " + self.descricao
+        # Alterado
+        return f'{self.codigo} - {self.descricao}'
